56e4948357e046b5573b081e8b5692c5a0cea4b0
Brook Seyoum <brookslmn@gmail.com>
2022-05-28 01:25:18 -0400

    Time: 108 ms (47.60%), Space: 15.3 MB (24.35%) - LeetHub

---
+++ 1-two-sum/1-two-sum.py
@@ -0,0 +1,28 @@
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         for i in range(len(nums)):
+#             for j in range(i+1,len(nums)):
+#                 if nums[i] + nums[j] == target:
+#                     return [i,j]
+
+
+# Above is the brute force method for finding the two sum of our target value
+# BigO : O(n**2)
+
+# We can solve this question using a hashmap
+# we loop through the array and subtract the target from our current value and the difference is the value we are looking for
+# we the check if the value is in the hashmap if it is we return an array of the 2 indexes (initial value, difference)
+# if its not we add the value and the index to our hashmap
+# BigO: O(n)
+
+
+# Neetcode => https://www.youtube.com/watch?v=KLlXCFG5TnA
+                    
+class Solution:
+   def twoSum(self, nums: List[int], target: int) -> List[int]: 
+        prevMap = {}
+        for index, value in enumerate(nums):
+           difference = target - value
+           if difference in prevMap:
+               return [index, prevMap[difference]]
+           prevMap[value] = index 
