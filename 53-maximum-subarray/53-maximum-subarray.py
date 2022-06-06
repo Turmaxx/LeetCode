c8197714d3469be80b3d52abc211e1948f690461
Brook Seyoum <brookslmn@gmail.com>
2022-06-06 15:14:13 -0400

    Time: 813 ms (38.37%), Space: 25.7 MB (48.33%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        maxSub =  nums[0]
+        curSum = 0
+        
+        for i in nums:
+            if curSum < 0:
+                curSum = 0
+            curSum += i
+            maxSub = max(maxSub, curSum)
+        return maxSub
