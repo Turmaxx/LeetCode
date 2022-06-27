d57758e8093a7ad4cfa087f4b6c99d885c7d7e61
Brook Seyoum <88465473+brook-seyoum@users.noreply.github.com>
2022-06-27 10:39:41 -0400

    Time: 153 ms (43.08%), Space: 14.4 MB (23.39%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -7,6 +7,9 @@ class Solution(object):
         """
         i, j = 0, len(numbers) - 1
         while i < j:
+            # return indices if sum of the 2 values is equal to target
             if numbers[i] + numbers[j] == target: return [i+1, j+1]
+            # if the sum is greater then the target decrement the second pointer by one since the array is sorted.
             elif numbers[i] + numbers[j] > target: j -= 1
+            # else if sum is less than the target increment the first pointer by one since the array is sorted.
             else: i += 1
