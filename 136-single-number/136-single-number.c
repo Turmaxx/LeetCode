37c46445d8050187195f29d9085e4eecd6392517
Brook Seyoum <88465473+brook-seyoum@users.noreply.github.com>
2022-07-09 16:07:18 -0400

    Time: 31 ms (46.24%), Space: 7.4 MB (29.02%) - LeetHub

--- 136-single-number/136-single-number.c
+++ 136-single-number/136-single-number.c
@@ -1,7 +1,5 @@
 int singleNumber(int* nums, int numsSize){
     int ans = 0;
-    for (int i = 0; i < numsSize; i++){
-        ans ^= nums[i];
-    }
+    for (int i = 0; i < numsSize; i++) ans ^= nums[i];
     return ans;
 }
