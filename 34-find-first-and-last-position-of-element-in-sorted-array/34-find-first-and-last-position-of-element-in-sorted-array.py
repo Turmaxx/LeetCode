56589d4e8bfb0cbd3b14ca4e48f238d880a5eda3
Brook Seyoum <88465473+brook-seyoum@users.noreply.github.com>
2022-07-10 17:11:49 -0400

    Time: 161 ms (21.21%), Space: 15.5 MB (10.28%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,23 +1,13 @@
 class Solution:
-    def bsearch(self, nums, target, found):
+    def search(self, nums, target):
         low, high = 0, len(nums) - 1
         
-        index = -1
-        while low <= high:
-            mid = int(low + (high - low) / 2)
-            if nums[mid] == target:
-                index = mid
-                if found: high = mid - 1
-                else: low = mid + 1
-            elif nums[mid] < target: low = mid + 1
-            else: high = mid - 1             
+        while nums[low] != target or nums[high] != target:
+            if nums[low] != target: low += 1
+            if nums[high] != target: high -= 1
             
-        return index
-        
-    def searchRange(self, nums: List[int], target: int) -> List[int]:        
-        if target not in nums or len(nums) == 0: return [-1,-1]
-        
-        index1 = self.bsearch(nums, target, True)    
-        index2 = self.bsearch(nums, target, False)
+        return [low, high]
     
-        return [index1, index2] 
+    def searchRange(self, nums, target):
+        if target not in nums or len(nums) == 0: return [-1,-1]
+        return self.search(nums, target)
