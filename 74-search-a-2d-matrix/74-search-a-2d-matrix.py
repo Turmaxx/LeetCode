5cb4e1cff56aa586e4bbdad56a1dfd404abdd6c5
Brook Seyoum <88465473+brook-seyoum@users.noreply.github.com>
2022-07-11 20:24:00 -0400

    Time: 93 ms (10.74%), Space: 14.3 MB (89.00%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -1,13 +1,14 @@
-class Solution(object):
-    def searchMatrix(self, matrix, target):
-        """
-        :type matrix: List[List[int]]
-        :type target: int
-        :rtype: bool
-        """
-        # for subarray in matrix:
-        #     if target in subarray:
-        #         return True
-        # return False
-        return any(target in row for row in matrix)
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        M, N = len(matrix), len(matrix[0])
+        r, c = 0, N - 1
+        
+        while r < M and c > -1:
+            if matrix[r][c] == target: return True;
+            elif matrix[r][c] > target: c -= 1
+            else: r += 1
+        
+        return False
+
     
+        # return any(target in sub for sub in matrix)
